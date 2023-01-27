5271bb5d917a0b0e863d337a6f6a59bdc7f1978e
Anh Truong <70667505+anhddt@users.noreply.github.com>
2023-01-27 00:47:45 -0500

    Create login form

--- frontend/src/components/loginForm/LoginForm.js
+++ frontend/src/components/loginForm/LoginForm.js
@@ -1,20 +1,70 @@
 import "./loginForm.css";
-import TextField from "@mui/material/TextField";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import FormControl from "@mui/material/FormControl";
-import { useState } from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import { useMemo, useState } from "react";
 
 const LoginPage = () => {
-  const [isDisabled, setIsDisabled] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const isDisabled = useMemo(() => {
+    return username === "" || password === "";
+  }, [username, password]);
+
   return (
     <FormControl class="login-form">
-        <Typography id="login-form-message" variant="h4">Welcome</Typography>
-        <TextField label="Username" variant="outlined" required />
-        <TextField label="Password" variant="outlined" required />
-        <Button color="primary" variant="contained" disabled={isDisabled}>Login</Button>
+      <Typography id="login-form-message" variant="h4">
+        Welcome
+      </Typography>
+      <Box>
+        Not a member? <Link>Create Account</Link>
+      </Box>
+      <Box className="field-container">
+        <PersonOutlineOutlinedIcon sx={{ fontSize: 50 }} />
+        <TextField
+          fullWidth
+          label="Username"
+          variant="outlined"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          required
+        />
+      </Box>
+      <Box className="field-container">
+        <LockOutlinedIcon sx={{ fontSize: 50 }} />
+        <TextField
+          fullWidth
+          label="Password"
+          variant="outlined"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          required
+        />
+      </Box>
+      <Button color="primary" variant="contained" disabled={isDisabled}>
+        Login
+      </Button>
+      <Grid container>
+        <Grid item xs={6}>
+          <Link>Forgot Password</Link>
+        </Grid>
+        <Grid item xs={6}>
+          <Link>Forgot Username</Link>
+        </Grid>
+      </Grid>
     </FormControl>
   );
-}
+};
 
 export default LoginPage;
