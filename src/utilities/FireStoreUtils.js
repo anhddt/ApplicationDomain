943712faaff593ba00084eb9188302d5ddeed259
Harrison Brown <70150559+sqKweglord@users.noreply.github.com>
2023-02-14 18:47:14 -0500

    added method to get users' role

--- src/utilities/FireStoreUtils.js
+++ src/utilities/FireStoreUtils.js
@@ -1,4 +1,4 @@
-import { collection, doc , setDoc, getDocs, updateDoc, deleteDoc, query, where } from 'firebase/firestore';
+import { collection, doc , setDoc, getDocs, getDoc, updateDoc, deleteDoc, query, where } from 'firebase/firestore';
 const {firestore} = require("../firebase.js");
 const db = firestore;
 
@@ -33,6 +33,14 @@ function getUserName(userInfo) {
 	return name;
 }
 
+
+//a function to get a users roloe
+export const getUserRole = async (uid) => {
+	const docSnap = await getDoc(doc(db, "newUsers", uid));
+	const role = docSnap.data.role;
+	return role;
+}
+
 //a function to retrieve the data of current users
 //returns a map with the generated username as the key
 export const getUsers = async () => {
@@ -42,13 +50,8 @@ export const getUsers = async () => {
 		console.log("query just passed");
 		let docs = querySnapshot.docs();
 		docs.forEach(function(doc) {
-			userMap.set(doc.username, doc);
-		})
-		/*
-		querySnapshot.forEach((doc) => {
-				userMap.set(doc.data.username, doc.data());		
+			userMap.set(doc.username, doc.data());
 		});
-		*/
 		console.log("set data values into map");
 		return userMap;
 	} catch (error) {
