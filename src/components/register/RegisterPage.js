cde8790d66ee74ac43b2d4d8874cec08396145f4
Anh Truong <70667505+anhddt@users.noreply.github.com>
2023-02-13 22:08:39 -0500

    U icleanup (#29)
    * fix ci pipe
    
    * test (#9)
    
    * Deploy (#15)
    
    * added firebase support
    
    * fixed firebase hosting
    
    * created deploy.yml for ci/cd
    
    * Create nav system
    
    * fix ci pipe
    
    * fix ci pipe
    
    * fix ci pipe
    
    * fix ci pipe
    
    * test (#9)
    
    * fix ci pipe
    
    * fix ci pipe
    
    * removed frontend folder, fixed pipe
    
    * upgrade node version, fix ci pipe (#10)
    
    * Users and roles (#11)
    
    * upgrade node version, fix ci pipe
    
    * update .gitignore
    
    * Deploy (#12)
    
    * upgrade node version, fix ci pipe
    
    * update .gitignore
    
    * upgrade node version to 16.4.0
    
    * Deploy (#13)
    
    * upgrade node version, fix ci pipe
    
    * update .gitignore
    
    * upgrade node version to 16.4.0
    
    * test (#9)
    
    * test
    
    * fix ci pipe
    
    * make main default for deploying
    
    * make main default for deploying
    
    ---------
    
    Co-authored-by: sqKweglord <70150559+sqKweglord@users.noreply.github.com>
    
    * rebase 1
    
    * rebase2
    
    * rebase3
    
    * rebase4
    
    * rebase5
    
    ---------
    
    Co-authored-by: sqKweglord <70150559+sqKweglord@users.noreply.github.com>

--- src/components/register/RegisterPage.js
+++ src/components/register/RegisterPage.js
@@ -1,6 +1,7 @@
 import "./registerPage.css";
 import { useMemo, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import { Header } from "../common";
 import { Box, Button, TextField, Typography } from "@mui/material";
 import { createAccount } from "../../utilities/utils";
 import {
@@ -57,6 +58,7 @@ const RegisterPage = () => {
 
   return (
     <Box className="container">
+      <Header />
       <Box className="userInfo-form">
         <Typography id="create-your-account-title" variant="h4">
           Create your account
