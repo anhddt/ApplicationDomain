fbb0ca67e50c3c5838c713968a274d4ae42f4743
IraGfC <ahaleogden@hotmail.com>
2023-02-15 14:21:09 -0500

    adminPage userInfo call

--- src/components/adminPage/AdminPage.js
+++ src/components/adminPage/AdminPage.js
@@ -1,14 +1,25 @@
 import './adminpage.css';
 import Box from "@mui/material/Box";
 import { Header } from "../common";
+import { getUserInfo } from '../../middleware/verification/userInfo';
+
 
 const AdminPage = () => {
-  
+
+    const users = async () => { 
+      const userInfo = await getUserInfo ();
+      //change console log to whatever function is needed to process all userInfo
+      //console.log (userInfo);
+    }
+    
+  users ()
+
+
     return (
       <Box className="screen">
         <Header />
         <Box className="adminBody">
-
+          
         </Box>
       </Box>
     );
