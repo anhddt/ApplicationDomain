fcc3c44a518ba8c6f904121783c04f4dec5da219
Anh Truong <70667505+anhddt@users.noreply.github.com>
2023-02-22 03:08:48 -0500

    added email confirmation

--- src/components/register/RegisterPage.js
+++ src/components/register/RegisterPage.js
@@ -10,6 +10,7 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
+import { showIf } from "../utils/conditionalRendering";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import { useAuth } from "../utils/AuthProvider";
@@ -21,7 +22,11 @@ import {
 } from "../../middleware/verification/userInfo";
 
 const RegisterPage = () => {
+  const date = new Date();
   const { createAccount } = useAuth();
+  const day = date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`;
+  const month =
+    date.getMonth() < 10 ? `0${date.getMonth() + 1}` : `${date.getMonth()}`;
   const [inputs, setInputs] = useState({
     firstName: "",
     lastName: "",
@@ -35,15 +40,13 @@ const RegisterPage = () => {
     zip: "",
     country: "",
     phone: "",
+    dateCreated: `${month}/${day}/${date.getFullYear()}`
   });
   const [confirmPw, setConfirmPw] = useState("");
-  const [confirmPwError, setConfirmPwError] = useState(true);
+  const [confirmPwError, setConfirmPwError] = useState(false);
   const [error, setError] = useState(false);
   const [focus, setFocus] = useState(false);
-  const date = new Date();
-  const day = date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`;
-  const month =
-    date.getMonth() < 10 ? `0${date.getMonth() + 1}` : `${date.getMonth()}`;
+  const [isEmailSent, setIsEmailSent] = useState(false);
   const navigateTo = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
   const handleClickShowPassword = () => setShowPassword((show) => !show);
@@ -75,9 +78,7 @@ const RegisterPage = () => {
    * comparison.
    */
   useMemo(() => {
-    if (inputs.password && confirmPw) {
-      setConfirmPwError(!(inputs.password === confirmPw));
-    }
+    setConfirmPwError(!(inputs.password === confirmPw));
     if (inputs.firstName && inputs.lastName) {
       const setUsername = () => {
         setInputs((existing) => ({
@@ -100,6 +101,7 @@ const RegisterPage = () => {
   };
   const handleSubmit = () => {
     createAccount(inputs, navigateTo, setError);
+    setIsEmailSent(!error);
   };
 
   /**
@@ -125,273 +127,281 @@ const RegisterPage = () => {
 
   return (
     <Box className="container">
-      <form className="userInfo-form">
-        <Typography id="create-your-account-title" variant="h4">
-          Create your account
-        </Typography>
-        <Box>
-          Already have an account?{" "}
-          <NavLink to="/login" className="nav-link">
-            Login instead
-          </NavLink>
+      {showIf(isEmailSent,
+        <Box sx={{backgroundColor: "white", borderRadius: "20px", padding: "20px"}} display="flex" flexDirection="column" alignItems="center">
+          <Typography textAlign="center" variant="subtitle1"> A verification email has been sent to <br/>{inputs.email}<br/>Please verify your email to login.</Typography>
+          <Button variant="contained" onClick={() => {navigateTo("/login")}}>OK</Button>
         </Box>
-        <Grid spacing={2} container>
-          <Grid item xs={12}>
-            <Typography textAlign="left" variant="h6">
-              Contact info:
-            </Typography>
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              name="firstName"
-              label="First Name"
-              value={inputs.firstName}
-              fullWidth
-              required
-              size="small"
-              variant="outlined"
-              onChange={(e) => {
-                handleChange(e);
-              }}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              name="lastName"
-              label="Last Name"
-              value={inputs.lastName}
-              fullWidth
-              required
-              size="small"
-              variant="outlined"
-              onChange={(e) => {
-                handleChange(e);
-              }}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              name="phone"
-              label="Phone number"
-              value={inputs.phone}
-              fullWidth
-              size="small"
-              variant="outlined"
-              onChange={(e) => {
-                handleChange(e);
-              }}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              name="street"
-              label="Street"
-              value={inputs.street}
-              fullWidth
-              size="small"
-              variant="outlined"
-              onChange={(e) => {
-                handleChange(e);
-              }}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              name="city"
-              label="City"
-              value={inputs.city}
-              fullWidth
-              size="small"
-              variant="outlined"
-              onChange={(e) => {
-                handleChange(e);
-              }}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              name="state"
-              label="State"
-              value={inputs.state}
-              fullWidth
-              size="small"
-              variant="outlined"
-              onChange={(e) => {
-                handleChange(e);
-              }}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              name="zip"
-              label="Zip"
-              value={inputs.zip}
-              fullWidth
-              size="small"
-              variant="outlined"
-              onChange={(e) => {
-                handleChange(e);
-              }}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              name="country"
-              label="Country"
-              value={inputs.country}
-              fullWidth
-              size="small"
-              variant="outlined"
-              onChange={(e) => {
-                handleChange(e);
-              }}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Typography textAlign="left" variant="h6">
-              Login info:
-            </Typography>
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              name="username"
-              label="Assigned Username"
-              value={inputs.username}
-              required
-              fullWidth
-              size="small"
-              variant="outlined"
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              name="email"
-              label="Email"
-              value={inputs.email}
-              required
-              fullWidth
-              size="small"
-              variant="outlined"
-              onChange={(e) => {
-                handleChange(e);
-              }}
-              error={error}
-              helperText={error && "Email invalid or already in use"}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              name="password"
-              label="Password"
-              value={inputs.password}
-              required
-              fullWidth
-              size="small"
-              variant="outlined"
-              type={showPassword ? "text" : "password"}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-              onChange={(e) => {
-                handleChange(e);
-              }}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              label="Confirm Password"
-              required
-              fullWidth
-              onFocus={() => {
-                handleFocus();
-              }}
-              size="small"
-              variant="outlined"
-              type={showPassword ? "text" : "password"}
-              error={confirmPwError && focus}
-              helperText={focus && confirmPwError && "Password does not match"}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-              onChange={(e) => {
-                setConfirmPw(e.target.value);
-              }}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Box id="password-constrains">
-              <Typography
-                color={checkPwLength(inputs.password) ? "green" : "black"}
-                display="block"
-                textAlign="left"
-                variant="contained"
-              >
-                Must be at least 8 characters.
+      )}
+      {showIf(!isEmailSent,
+        <form className="userInfo-form">
+          <Typography id="create-your-account-title" variant="h4">
+            Create your account
+          </Typography>
+          <Box>
+            Already have an account?{" "}
+            <NavLink to="/login" className="nav-link">
+              Login instead
+            </NavLink>
+          </Box>
+          <Grid spacing={2} container>
+            <Grid item xs={12}>
+              <Typography textAlign="left" variant="h6">
+                Contact info:
               </Typography>
-              <Typography
-                color={checkPwFirstChar(inputs.password) ? "green" : "black"}
-                display="block"
-                textAlign="left"
-                variant="contained"
-              >
-                Must start with a letter.
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                name="firstName"
+                label="First Name"
+                value={inputs.firstName}
+                fullWidth
+                required
+                size="small"
+                variant="outlined"
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                name="lastName"
+                label="Last Name"
+                value={inputs.lastName}
+                fullWidth
+                required
+                size="small"
+                variant="outlined"
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                name="phone"
+                label="Phone number"
+                value={inputs.phone}
+                fullWidth
+                size="small"
+                variant="outlined"
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                name="street"
+                label="Street"
+                value={inputs.street}
+                fullWidth
+                size="small"
+                variant="outlined"
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                name="city"
+                label="City"
+                value={inputs.city}
+                fullWidth
+                size="small"
+                variant="outlined"
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                name="state"
+                label="State"
+                value={inputs.state}
+                fullWidth
+                size="small"
+                variant="outlined"
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                name="zip"
+                label="Zip"
+                value={inputs.zip}
+                fullWidth
+                size="small"
+                variant="outlined"
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                name="country"
+                label="Country"
+                value={inputs.country}
+                fullWidth
+                size="small"
+                variant="outlined"
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography textAlign="left" variant="h6">
+                Login info:
               </Typography>
-              <Typography
-                color={checkPwForNumbers(inputs.password) ? "green" : "black"}
-                display="block"
-                textAlign="left"
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                name="username"
+                label="Assigned Username"
+                value={inputs.username}
+                required
+                fullWidth
+                size="small"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                name="email"
+                label="Email"
+                value={inputs.email}
+                required
+                fullWidth
+                size="small"
+                variant="outlined"
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+                error={error}
+                helperText={error && "Email invalid or already in use"}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                name="password"
+                label="Password"
+                value={inputs.password}
+                required
+                fullWidth
+                size="small"
+                variant="outlined"
+                type={showPassword ? "text" : "password"}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                label="Confirm Password"
+                required
+                fullWidth
+                onFocus={() => {
+                  handleFocus();
+                }}
+                size="small"
+                variant="outlined"
+                type={showPassword ? "text" : "password"}
+                error={confirmPwError && focus}
+                helperText={focus && confirmPwError && "Password does not match"}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
+                onChange={(e) => {
+                  setConfirmPw(e.target.value);
+                }}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Box id="password-constrains">
+                <Typography
+                  color={checkPwLength(inputs.password) ? "green" : "black"}
+                  display="block"
+                  textAlign="left"
+                  variant="contained"
+                >
+                  Must be at least 8 characters.
+                </Typography>
+                <Typography
+                  color={checkPwFirstChar(inputs.password) ? "green" : "black"}
+                  display="block"
+                  textAlign="left"
+                  variant="contained"
+                >
+                  Must start with a letter.
+                </Typography>
+                <Typography
+                  color={checkPwForNumbers(inputs.password) ? "green" : "black"}
+                  display="block"
+                  textAlign="left"
+                  variant="contained"
+                >
+                  Must have a number.
+                </Typography>
+                <Typography
+                  color={
+                    checkPwForSpecialChar(inputs.password) ? "green" : "black"
+                  }
+                  display="block"
+                  textAlign="left"
+                  variant="contained"
+                >
+                  Must have a special character (!, @, #, $, %, *, etc.).{" "}
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                fullWidth
                 variant="contained"
+                disabled={isDisabled || confirmPwError}
+                onClick={() => {
+                  handleSubmit();
+                }}
               >
-                Must have a number.
-              </Typography>
-              <Typography
-                color={
-                  checkPwForSpecialChar(inputs.password) ? "green" : "black"
-                }
-                display="block"
-                textAlign="left"
-                variant="contained"
-              >
-                Must have a special character (!, @, #, $, %, *, etc.).{" "}
-              </Typography>
-            </Box>
+                Submit
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <Button
-              fullWidth
-              variant="contained"
-              disabled={isDisabled || confirmPwError}
-              onClick={() => {
-                handleSubmit();
-              }}
-            >
-              Submit
-            </Button>
-          </Grid>
-        </Grid>
-      </form>
+        </form>
+      )}
     </Box>
   );
 };
