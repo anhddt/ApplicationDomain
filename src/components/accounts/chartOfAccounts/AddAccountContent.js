5de90eb06a37e1e9055c6ff866f79c9da71d5ce2
Anh Truong <anhddt@yahoo.com>
2023-03-22 13:00:10 -0400

    adding textfields in add account

--- src/components/accounts/chartOfAccounts/AddAccountContent.js
+++ src/components/accounts/chartOfAccounts/AddAccountContent.js
@@ -7,6 +7,7 @@ import {
   StepLabel,
   Stepper,
   Typography,
+  TextField,
 } from "@mui/material";
 import { showIf } from "../../utils/conditionalRendering";
 
@@ -19,14 +20,47 @@ const steps = ["Account name?", "Category", "Subcategory", "Balance"];
 const AddAccountContent = () => {
   // This is for indicating which step the user is currently on.
   const [currentStep, setCurrentStep] = useState(0);
-  const handleCancel = async () => {
+  const [newAccount, setNewAccount] = useState({
+    id: 1,
+    name: "",
+    category: "",
+    subCat: "",
+    balance:"",
+  });
+  const getName = (index) => {
+    if(index === 0) return "name";
+    else if(index === 1) return "category";
+    else if(index === 2) return "subCat";
+    else return "balance";
+  }
+  const getPlaceholder = (index) => {
+    if(index === 0) return "Account name";
+    else if(index === 1) return "Account category";
+    else if(index === 2) return "Account sub-category";
+    else return "Account balance";
+  }
+  const getValue = (index) => {
+    if(index === 0) return newAccount.name;
+    else if(index === 1) return newAccount.category;
+    else if(index === 2) return newAccount.subCat;
+    else return newAccount.balance;
+  }
+  const handleCancel = () => {
     setCurrentStep(0);
+  };
+  // Every time the user types, handle the change here
+  const handleChange = (e) => {
+    setNewAccount(rest => ({
+      ...rest,
+      [e.target.name]: e.target.value,
+    }));
   };
   // After finishing creating the account,
   // The page reload and the step is reset back to 0.
-  const finish = async () => {
+  const finish = () => {
+    console.log(newAccount);
     handleCancel();
-    window.location.reload();
+    // window.location.reload();
   };
   const next = (index) => {
     if (index === steps.length - 1) {
@@ -45,6 +79,7 @@ const AddAccountContent = () => {
       </StepLabel>
       <StepContent>
         <Box sx={{ display: "flex", mb: 2, gap: "10px" }}>
+          <TextField sx={{mb: "10px", width: "200px"}} name={getName(index)} value={getValue(index)} placeholder={getPlaceholder(index)} onChange={(e) => handleChange(e)} size="small"></TextField>
           <Button variant="contained" onClick={() => next(index)}>
             {index === steps.length - 1 ? "Finish" : "Next"}
           </Button>
