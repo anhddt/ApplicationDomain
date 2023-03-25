0420f0c0610e8de4774d05ee94e9d6b27a1dfb95
IraGfC <ahaleogden@hotmail.com>
2023-03-25 13:23:02 -0400

    Update AccountDetail.js
    Placeholder table.

--- src/components/accountDetailPage/AccountDetail.js
+++ src/components/accountDetailPage/AccountDetail.js
@@ -6,6 +6,16 @@ import Homebar from "../common/header/Homebar";
 
 const AccountDetail = () => {
     const { theme } = useThemeProvider();
+
+    //Temp dictionary to fill tables
+    const sample1 = [
+        {transNum: "201", transDesc: "Placeholder", transDate: "11/16/2022", transDebit: "+100", transCredit: "-100"},
+        {transNum: "202", transDesc: "Placeholder", transDate: "08/09/2022", transDebit: "+1000", transCredit: "-1000"},
+        {transNum: "203", transDesc: "Placeholder", transDate: "03/15/2022", transDebit: "-200", transCredit: "+200"},
+        {transNum: "204", transDesc: "Placeholder", transDate: "02/21/2022", transDebit: "-2000", transCredit: "+2000"},
+
+    ];
+
     return (
         <Paper>
             <Homebar />
@@ -14,10 +24,33 @@ const AccountDetail = () => {
                 id={theme === "dark" ? "paper-dark" : "paper-light"}
             >
                 <Box
-                className="acctDetailBody"
-                id={theme === "dark" ? "box-dark" : "box-light"}
+                    className="acctDetailBody"
+                    id={theme === "dark" ? "box-dark" : "box-light"}
                 >
-
+                    <div className="userTable">
+                        <table>
+                        <tbody>
+                            <tr>
+                            <th>Transaction #</th>
+                            <th>Transaction Description</th>
+                            <th>Transaction Date</th>
+                            <th>Debit</th>
+                            <th>Credit</th>
+                            </tr>
+                            {sample1.map((val, key) => {
+                            return (
+                                <tr key={key}>
+                                <td>{val.transNum}</td>
+                                <td>{val.transDesc}</td>
+                                <td>{val.transDate}</td>
+                                <td>{val.transDebit}</td>
+                                <td>{val.transCredit}</td>
+                                </tr>
+                            );
+                            })}
+                        </tbody>
+                        </table>
+                    </div>
 
                 </Box>
             </Box>
